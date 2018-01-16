3ec3f734a2b359903d43d419b232dc6a99e485a8
Gideao <gideao@Laptop-Lenovo-G400s.(none)>
2018-01-16 18:10:43 -0200

    version 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello world - 2'))
+app.get('/', (req, res) => res.send('Hello world - 3'))
 
 app.listen(process.env.PORT || 3000, err => console.log(err || 'Running...'))
