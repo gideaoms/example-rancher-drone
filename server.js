e7937f24468a018cad2f7ec1ef5499b8a166229c
Gideao <gideao@Laptop-Lenovo-G400s.(none)>
2018-01-16 11:39:01 -0200

    added files from project

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello world'))
+
+app.listen(process.env.PORT || 3000, err => console.log(err || 'Running...'))
